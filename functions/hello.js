3807f7d540adbfd4ffd346ceb2cd378adc7ca307
Dorell James <galangdj@gmail.com>
2020-11-14 20:19:00 +0800

    Updated examples

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,20 @@
 exports.handler = async (event, context) => {
+  let data = {};
+
+  if (event.httpMethod === "POST") {
+    try {
+      data = JSON.parse(event.body);
+    } catch (err) {
+      console.log("Unable to parse event body! Are you passing it correctly?");
+    }
+  }
+  console.log("data", data);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Serverless Functions are cool! 😎" }),
+    body: JSON.stringify({
+      message: "Serverless Functions are cool! 😎",
+      data,
+    }),
   };
 };
